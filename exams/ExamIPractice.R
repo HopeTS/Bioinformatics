4a3ef3af994bedbafc1ced64185bc1145ae49f01
Robert Bergers <bergersr@my.easternct.edu>
2019-09-27 10:24:17 -0400

    spacing for readibility

--- exams/ExamIPractice.R
+++ exams/ExamIPractice.R
@@ -19,7 +19,6 @@ library(readr)
 
 
 # 1. Create a vector called ages that contains the numbers 21, 24, 32, and 19
-
 ages <- c(21, 24, 32, 19)
 
 # 2. Create a vector called evens that contains all even numbers 
@@ -27,10 +26,10 @@ ages <- c(21, 24, 32, 19)
 evens <- c(seq(2, 100, 2), 200)
 evens
 
+
 # 3. Write a function called min.positive which takes a vector 'x' as an
 #    argument and returns the the smallest positive number from the vector.
 #    For example, for v <- c(-3,10,2), min.positive(v) would return 2
-
 min.positive <- function(x) {
   min(x[x>=0])
 }
@@ -50,31 +49,29 @@ min.max(ages)
 
 # 5. Include the following code in your script to create a matrix filled with 
 #    5 columns and 20 rows, that is filled with random numbers between 0 and 1.
-
       m = matrix(runif(100), ncol = 5, nrow = 20)
       ran.m <- matrix(sample(0:1, 100, replace=T), ncol = 5, nrow = 20)
       ran.m
-
+      
 #   (a) Find the median of each row 
       apply(ran.m, 1, median)
+
 #   (b) Find the median of each column.
       apply(ran.m, 2, median)
 
-# 6. Run the command below to read in an old class survey:
       
+# 6. Run the command below to read in an old class survey:
       survey <- read.delim("http://pastebin.com/raw/1csmBawE")
       
 # 7. The code below generates a scatterplot of College GPA against Alcohol
 #    consumption that includes the regression line, and colors the points by
 #    Gender.
-      
       ggplot(survey, aes(Alcohol, College.GPA)) + 
         geom_point(aes(color = Gender)) + 
         geom_smooth(method = 'lm', color = 'black', se = FALSE) +
         theme_classic() + ggtitle('Alcohol Consumption vs. College GPA') +
         xlab('Alcohol consumption (days / week)')
 
-    
     # (a) Fit a linear model that predicts college GPA from Alcohol consumption
     #     (i)  Find and interpret the y-intercept
       fit <- lm(survey$College.GPA ~ survey$Alcohol)
@@ -91,12 +88,12 @@ min.max(ages)
       survey.9 <- filter(survey, Marijuana.Legalization == 'Agree')
       mean(survey.9$College.GPA)    # ~ 3
       sd(survey.9$College.GPA)    # 0.45
-      
+
+            
 # 10. Construct side-by-side boxplots for FB usage based on whether or not
 #    a person agrees or disagrees with same sex marriage 
 #    Was there an association between FB usage and views on same sex marriage
 #    in this class?
-      
       ggplot(m) + geom_col(aes(type, value, fill = presence)) +
         labs(y = "Proportion", fill = "Pesticide status", 
              title = "Distribution of pesticide status by food type") +
