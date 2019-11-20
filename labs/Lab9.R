e7a9bd90c90c061ed335493a329a3ddea17434db
Robert Bergers <bergersr@my.easternct.edu>
2019-11-19 19:21:50 -0500

    do lab 9

--- labs/Lab9.R
+++ labs/Lab9.R
@@ -37,6 +37,8 @@ library(affy)
   s.dog <- mean(s$Dog)
   s.sleep <- s.dog - s.cat
   s.sleep
+  
+  print('A p value of 0.79 Shows there isn\'t a statistically significant difference between mean hours of sleep for cat and dog people')
 
 ##################################################################################
 # 2.Fit a linear model that predicts Hours of Sleep based on 
@@ -47,12 +49,16 @@ library(affy)
 # (a) Find and interpret the y-intercept of the regression line in the
 #      context of this problem.'
 
-  
+  dog.person <- as.integer(survey$CatOrDogPerson == "Dog")
+  a.2 <- lm(survey$Sleep ~ dog.person, data = survey)
+  a.2
+
+  print('The y intercept is 6.3 hrs of sleep.')  
 
 # (b) Find and interpret the slope of the regression line in the context of 
 #     this problem
-
-
+  
+  print('The slope of the regression line is 0.2')
 
 # (c) What is the p-value for the hypothesis test that there is a
 #     significant difference in Hours of Sleep between the two groups?
@@ -60,7 +66,9 @@ library(affy)
 #     from the linear model should match the p-value from the two-sample 
 #     t-test from problem 1(a) above.
 ##################################################################################
-
+  
+  summary(a.2)
+  print('The p value of 0.79 shows there is no statistically significant difference')
 
 ###############################################################
 # 3. Get the processed data for GSE19143 and pull out the 
@@ -73,17 +81,19 @@ library(affy)
 #    (non-responsive) to this drug. 
 ###############################################################
 
-# GSE19143 <- getGEO("GSE19143")
+  GSE19143 <- getGEO("GSE19143")
 # GSE19143.rma <- rma(GSE19143)
-  GSE19143 <- ReadAffy(celfile.path = "c:/Users/berge/Documents/GitHub/Bioinformatics/labs/GSM_DATA_2/")
-  GSE19143.rma <- rma(GSE19143)
-  GSE19143.expr <- exprs(GSE19143.rma)
+  GSE19143.affy <- ReadAffy(celfile.path = "c:/Users/berge/Documents/GitHub/Bioinformatics/labs/GSM_DATA_2/")
+#  GSE19143.rma <- rma(GSE19143)
+  GSE19143.expr <- exprs(GSE19143[[1]])
+  GSE19143.expr <- log2(GSE19143.expr)
+  GSE19143.p <- pData(GSE19143[[1]])
   
-  View(GSE19143.expr)
+#  View(GSE19143.expr)
 
 # (a) How many samples had their gene expression values profiled?
   
-  ncol(GSE19143)
+  ncol(GSE19143.expr)
 
 # (b) How many probes are on the array?
   
@@ -94,12 +104,20 @@ library(affy)
 #     The analysis beginning with question 5 must use the log2 data; 
 #     otherwise the results will not be correct.
 
-
+  c.3 <- data.frame(GSE19143.expr, rownames(GSE19143.expr))
+  boxplot(GSE19143.expr)
+  
+  
 #####################################################################
 # 4.How many individuals are resistant to prednisolone and
 # how many are sensitive? 
 #####################################################################
 
+  prednisolone <- levels(GSE19143.p$characteristics_ch1.5) <- c("Resistant", "Sensitive")
+  prednisolone <- GSE19143.p$characteristics_ch1.5
+  table(prednisolone)
+  print('25 are resistant, 27 are sensitive')
+  
 #####################################################################
 # 5. Find the top differentially expressed probes, with a FDR of 10%,
 # between individuals that are resistant vs. sensitive to prednisolone.
@@ -109,7 +127,13 @@ library(affy)
 # expression) in resistant individuals. 
 #####################################################################
   
-  
+  matrix <- model.matrix(~0+prednisolone)
+  colnames(matrix) <- c("Resistant", "Sensitive")
+  fit <- lmFit(GSE19143.expr, matrix)
+  matrix.2 <- makeContrasts(Resistant - Sensitive, levels=matrix)
+  fit.2 <- contrasts.fit(fit, matrix.2)
+  fit.2 <- eBayes(fit)
+  fdr <- topTable(fit.2, p.value = 0.10, number = 16)
 
 ########################################################################
 # 6. Construct a heatmap of these top 16 probes, with individuals 
@@ -118,7 +142,13 @@ library(affy)
 # you may do this with the first 16 probes in the expression matrix).
 ########################################################################
 
-  
+  m <- match(rownames(fdr), rownames(GSE19143.expr))
+  m.2 <- GSE19143.expr[m,]
+  col.heat <- colorRampPalette(c("red", "green"))(200)
+  plot(1:200, col = col.heat, pch = 18, cex = 3)
+  col.resistance <- as.integer(as.factor(prednisolone))
+  col.resistance <- c("red", "green")[col.resistance]
+  heatmap(m.2, ColSideColors = col.resistance, col = col.heat)
   
 ########################################################################
 # 7. If you answered question 5 correctly, the SECOND hit 
@@ -128,6 +158,12 @@ library(affy)
 # associated with this probe. 
 #######################################################################
 
+  platform <- annotation(GSE19143[[1]])
+#  platform
+  pl <- getGEO(platform)
+  pl <- Table(pl)
+  m <- match("209374_s_at", pl$ID)
+  pl$`Gene Symbol`[m]
 
 #####################################################################
 # 8. How many probes are there for the gene IGHM on the platform
@@ -136,7 +172,10 @@ library(affy)
 # code must also output the number of probes.
 ####################################################################
 
-
+  probe <- grep("IGHM", pl$`Gene Symbol`)
+  df <- data.frame(pl[probe,])
+  nrow(df)
+  print('There are 34 probes')
 
 ########################################################################
 # Final Notes: the heatmap in question 6 provides a candidate list
