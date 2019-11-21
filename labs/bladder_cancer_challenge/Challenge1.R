9305e490702618e2940d4c56c5ef49d00773445d
Robert Bergers <bergersr@my.easternct.edu>
2019-11-21 15:24:58 -0500

    plot training set

--- labs/bladder_cancer_challenge/Challenge1.R
+++ labs/bladder_cancer_challenge/Challenge1.R
@@ -1,30 +1,29 @@
 library(limma)
 library(class)
-library(GEOquery)
 
-rm(list = ls()) ## this will delete all objects in the workspace
+  rm(list = ls()) ## this will delete all objects in the workspace
 
 ###################################################################
 # Useful functions
 ###################################################################
 
 ## scale rows of given matrix to have mean 0 and sd of 1
-row.scale <-function(x) {   #ensures one probe is considered more important than others
-  x.scale = t(scale(t(x)))
-  return(x.scale)
-}
+  row.scale <-function(x) {
+    x.scale = t(scale(t(x)))
+    return(x.scale)
+  }
 
 # returns the balanced accuracy
-balanced.accuracy <-function(predicted, true) { #
-  t <- table(true = true, predicted = predicted)
-  if (nrow(t) != 2) {
-    stop("invalid number of rows in accuracy table")
+  balanced.accuracy <-function(predicted, true) {
+    t <- table(true = true, predicted = predicted)
+    if (nrow(t) != 2) {
+      stop("invalid number of rows in accuracy table")
+    }
+    acc <- diag(t) / rowSums(t)
+    sens1 <- acc[1]
+    sens2 <- acc[2]
+    list(table = t, sensitivity1 = sens1, sensitivity2 = sens2, balanced.accuracy = mean(acc))
   }
-  acc <- diag(t) / rowSums(t)
-  sens1 <- acc[1]
-  sens2 <- acc[2]
-  list(table = t, sensitivity1 = sens1, sensitivity2 = sens2, balanced.accuracy = mean(acc))
-}
 
 
 ###################################################################
@@ -35,41 +34,19 @@ balanced.accuracy <-function(predicted, true) { #
 # Y.train - the class labels (NMI or MI) for the training samples
 # X.test - the log2 gene expression data for the test samples
 
-load(url("https://gdancik.github.io/CSC-315/data/hw/Challenge.RData"))
+  load(url("https://gdancik.github.io/CSC-315/data/hw/Challenge.RData"))
 
 # 1) Find differentially expressed probes in your training dataset
 
-X.train = log2(X.train)
-colnames(X.train) = c(as.character(Y.train))
-design = model.matrix(~0+Y.train)
-colnames(design) = c("MI", "NMI")
+  View(X.train)
+  View(Y.train)
+  
 
-fit = lmFit(X.train, design)
-
-contrast.matrix = makeContrasts(MI - NMI, levels=design)
-fit2 = contrasts.fit(fit, contrast.matrix)
-fit2 = eBayes(fit)
-
-FDR = topTable(fit2, p.value = 0.05)
-#FDR = topTable(fit2, p.value = 0.05, number = nrow(X.train))
 
 # 2) Using the differentially expressed probes, evaluate a knn classifier 
 #    using leave-one-out cross-validation in the training data set. 
 #    Don't forget to scale your data (training and testing). 
 #    Find the balanced accuracy.
-
-m = match(rownames(FDR), rownames(X.train))
-X = X.train[m,]
-
-col.muscleInvasion = as.integer(as.factor(Y.train))
-col.muscleInvasion = c("red", "blue")[col.muscleInvasion]
-X.scale = row.scale(X)
-
-df = data.frame(probe1 = X.scale[1,], probe2 = X.scale[2,], Y.train = Y.train)
-
-ggplot(df, aes(probe1,probe2, color = Y.train)) + geom_point() +
-  theme_classic() + ggtitle("Scaled expression of probes") +
-  scale_color_manual(values = c("red", "blue")) 
 
 # 3) Classify the test samples, and email your predictions to
 #    dancikg@easternct.edu with the subject: Bioinformatics Challenge. In the
